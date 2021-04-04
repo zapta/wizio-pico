128f69264dd7686c764e070def5ecce4dbcc9cd5
Georgi Angelov <the.wizarda@gmail.com>
2021-04-04 17:15:56 +0300

    Update main.cpp

--- examples/baremetal/pico-ardiuno/src/main.cpp
+++ examples/baremetal/pico-ardiuno/src/main.cpp
@@ -1,30 +1,4 @@
 #include <Arduino.h> 
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define SLAVE_ADDRESS 0x3C
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
-
-void oled_setup()
-{
-    display.begin(SSD1306_SWITCHCAPVCC, SLAVE_ADDRESS);
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.println("  Arduino");
-    display.setTextColor(BLACK, WHITE);
-    display.println(" Pi Pico  ");
-    display.println("  RP2040  ");
-    display.setTextSize(1);
-    display.setTextColor(WHITE, BLACK);
-    display.println();
-    display.println("      WizIO 2021");
-    display.display();
-}
 
 int main(void)
 {
